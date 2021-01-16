51dbab411e963407c244e921d2b43d495d46f1cd
Alexis Foerster <alexis.foerster@gmail.com>
2021-01-16 21:00:00 +0100

    Implémentation de la structure principale : Page.

---
+++ UtilitairesFinalFantasyXIV/Structure/Page.py
@@ -0,0 +1,60 @@
+# coding: utf-8
+
+# ==================================================================================================
+# Name        : Page.py
+# Author      : Alexis Foerster (alexis.foerster@gmail.com)
+# Version     : 3.0.0 (DD/MM/YYYY)
+# Description : Définition d'une page, d'une page d'élément et d'une page d'ensemble
+# ==================================================================================================
+
+from urllib import request
+from bs4 import BeautifulSoup
+from UtilitairesFinalFantasyXIV.Structure.Element import Element
+
+
+class Page(Element):
+
+    def __init__(self, nom=str(), adresse=str()):
+        super().__init__(nom)
+        self.adresse = adresse
+        self.texte = None
+        self.soup = None
+
+    def lire(self):
+        # TODO: Implémenter une nouvelle tentative en cas d'échec ?
+        self.texte = request.urlopen(self.adresse).read()
+        self.soup = BeautifulSoup(self.texte, "html.parser")
+
+    def extraire(self):
+        pass  # Note: Implémenter l'extraction pour chaque page
+
+    def recuperer(self):
+        pass  # Note: Implémenter la récupération pour chaque page
+
+    def traiter(self):
+        self.lire()
+        self.extraire()
+        return self.recuperer()
+
+    def afficher(self):
+        print(self.soup.prettify("utf-8"))
+
+
+class PageElement(Page):
+
+    def __init__(self, nom=str(), adresse=str()):
+        super().__init__(nom, adresse)
+        self.element = None
+
+    def recuperer(self):
+        return self.element
+
+
+class PageEnsemble(Page):
+
+    def __init__(self, nom=str(), adresse=str()):
+        super().__init__(nom, adresse)
+        self.ensemble = None
+
+    def recuperer(self):
+        return self.ensemble
